2cb81b9ef65af51d572b4e6dc509308dd02b833f
Alan B. Christie <alan@kaitak.co.uk>
2020-11-28 17:11:13 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+function updateindicator () {
+    if (newpitchrow != pitchrow || newrollcolumn != rollcolumn) {
+        if (rollcolumn >= 0 && pitchrow >= 0) {
+            led.unplot(rollcolumn, pitchrow)
+        }
+        led.plot(newrollcolumn, newpitchrow)
+        pitchrow = newpitchrow
+        rollcolumn = newrollcolumn
+    }
+}
+function setpitchrow () {
+    pitch = input.rotation(Rotation.Pitch)
+    if (pitch > 10) {
+        newpitchrow = 4
+    } else {
+        if (pitch > 5) {
+            newpitchrow = 3
+        } else {
+            if (pitch < -10) {
+                newpitchrow = 0
+            } else {
+                if (pitch < -5) {
+                    newpitchrow = 1
+                } else {
+                    if (pitch < 2 && pitch > -2) {
+                        newpitchrow = 2
+                    }
+                }
+            }
+        }
+    }
+}
+function setrollcolumn () {
+    roll = input.rotation(Rotation.Roll)
+    if (roll > 10) {
+        newrollcolumn = 4
+    } else {
+        if (roll > 5) {
+            newrollcolumn = 3
+        } else {
+            if (roll < -10) {
+                newrollcolumn = 0
+            } else {
+                if (roll < -5) {
+                    newrollcolumn = 1
+                } else {
+                    if (roll < 2 && roll > -2) {
+                        newrollcolumn = 2
+                    }
+                }
+            }
+        }
+    }
+}
+let roll = 0
+let pitch = 0
+let newrollcolumn = 0
+let newpitchrow = 0
+let rollcolumn = 0
+let pitchrow = 0
+pitchrow = -1
+rollcolumn = -1
+newpitchrow = 0
+newrollcolumn = 0
+updateindicator()
+basic.forever(function () {
+    setpitchrow()
+    setrollcolumn()
+    updateindicator()
+})
